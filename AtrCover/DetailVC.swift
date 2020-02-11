bc6c17b38d83b1fcc67a6f27c9d9f94b233308d7
Artem Ulko <ulkoart@gmail.com>
2020-02-11 15:57:39 +0300

    init

---
+++ AtrCover/DetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  DetailVC.swift
+//  AtrCover
+//
+//  Created by user on 11/02/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
