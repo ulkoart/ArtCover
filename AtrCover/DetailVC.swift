830f83d5105a4484de408b08dcc608ccbce49f47
Artem Ulko <ulkoart@gmail.com>
2020-02-11 15:58:12 +0300

    step 2

--- AtrCover/DetailVC.swift
+++ AtrCover/DetailVC.swift
@@ -10,21 +10,17 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var tracktitle = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        image.image = UIImage(named: tracktitle)
+        titleLabel.text = tracktitle
+        titleLabel.numberOfLines = 0
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- AtrCover/TrackList.swift
+++ AtrCover/TrackList.swift
@@ -9,6 +9,17 @@
 import UIKit
 
 class TrackList: UITableViewController {
+    
+    let imageNameArray = ["Alberto Ruiz - 7 Elements (Original Mix)",
+                          "Dave Wincent - Red Eye (Original Mix)",
+                          "E-Spectro - End Station (Original Mix)",
+                          "Edna Ann - Phasma (Konstantin Yoodza Remix)",
+                          "Ilija Djokovic - Delusion (Original Mix)",
+                          "John Baptiste - Mycelium (Original Mix)",
+                          "Lane 8 - Fingerprint (Original Mix)",
+                          "Mac Vaughn - Pink Is My Favorite Color (Alex Stein Remix)",
+                          "Metodi Hristov, Gallya - Badmash (Original Mix)",
+                          "Veerus, Maxie Devine - Nightmare (Original Mix)"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,27 +31,24 @@ class TrackList: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return imageNameArray.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Title", for: indexPath)
+        cell.imageView?.image = UIImage(named:imageNameArray[indexPath.row])
+        cell.textLabel?.text = imageNameArray[indexPath.row]
+        cell.textLabel?.numberOfLines = 0
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -77,14 +85,17 @@ class TrackList: UITableViewController {
     }
     */
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let detailVC = segue.destination as! DetailVC
+                detailVC.tracktitle = imageNameArray[indexPath.row]
+            }
+        }
     }
-    */
+
 
 }
